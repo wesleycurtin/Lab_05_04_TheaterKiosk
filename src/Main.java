23848b241929e84b19b2a03c1cab39df43efd855
WesleyCurtin <curtinwl@mail.uc.edu>
2022-09-26 18:31:51 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 12;
+
+        if (age >= 21) {
+            System.out.println("You said you were " + age + ". You get a wristband! Have Fun!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
